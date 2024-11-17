37e64732be58a225618d50f5e8bd7e09e5588726
wispl <wispl.8qbkk@slmail.me>
2024-11-17 02:34:36 -0500

    feat: update shaders

--- src/shaders.cpp
+++ src/shaders.cpp
@@ -30,37 +30,13 @@ constexpr std::string_view frag_shader = R"(
 	in vec2 texcoord;
 	out vec4 fragcolor;
 
-	// struct Material {
-	//
-	// };
-	// layout(location = 0) uniform sampler2D albedoTexture;
-	// layout(binding = 0, std140) uniform MaterialUniforms {
-	// 	vec4 baseColorFactor;
-	// 	float alphaCutoff;
-	// 	uint flags;
-	// } material;
-
-	// float rand(vec2 co) {
-	// 	return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
-	// }
-
-	// vec2 transformUv(vec2 uv) {
-	// 	mat2 rotationMat = mat2(
-	// 		cos(uvRotation), -sin(uvRotation),
-	// 		sin(uvRotation), cos(uvRotation)
-	// 	);
-	// 	return rotationMat * uv * uvScale + uvOffset;
-	// }
+	layout(location = 0) uniform sampler2D albedoTexture;
+	uniform vec4 base_color;
 
 	void main() {
-		// vec4 color = material.baseColorFactor;
-		// if ((material.flags & HAS_BASE_COLOR_TEXTURE) == HAS_BASE_COLOR_TEXTURE) {
-		// 	color *= texture(albedoTexture, transformUv(texCoord));
-		// }
-		// float factor = (rand(gl_FragCoord.xy) - 0.5) / 8;
-		// if (color.a < material.alphaCutoff + factor)
-		//    		discard;
-		fragcolor = vec4(0, 0, 0, 0);
+		vec4 color = base_color;
+		// color *= texture(albedo_texture, texcoord);
+		fragcolor = base_color;
 	}
 )";
 
