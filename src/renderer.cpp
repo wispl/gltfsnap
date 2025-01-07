fb41c7b6f4c114c2c5e700b79f281b5183e21fd5
wispl <wispl.8qbkk@slmail.me>
2025-01-07 15:08:40 -0500

    feat: update renderer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -2,6 +2,12 @@
 
 #include "gltf.h"
 
+template <typename T>
+static void merge(std::vector<T>& a, std::vector<T>& b) {
+	a.reserve(a.size() + b.size());
+	a.insert(a.end(), b.begin(), b.end());
+}
+
 Renderer::Renderer(GLuint program)
 {
 	glUseProgram(program);
@@ -20,27 +26,72 @@ Renderer::Renderer(GLuint program)
 
 	model_uniform = glGetUniformLocation(program, "model");
 	view_proj_uniform = glGetUniformLocation(program, "view_proj");
+	glCreateBuffers(1, &material_ubo);
+	glNamedBufferStorage(material_ubo, static_cast<GLsizeiptr>(sizeof(Material)), nullptr, GL_MAP_WRITE_BIT);
 
 	glEnable(GL_DEPTH_TEST);
 }
 
-void Renderer::render(Scene& scene) const
+void Renderer::update_scene(Scene& scene)
 {
-	for (auto& node : scene.nodes) {
-		glUniformMatrix4fv(model_uniform, 1, GL_FALSE, &node.transform[0][0]);
-		for (auto& mesh : node.gltf.meshes) {
-			// bind mesh buffers
-			glVertexArrayVertexBuffer(vao, 0, mesh.vertex_buffer, 0, sizeof(Vertex));
-			glVertexArrayElementBuffer(vao, mesh.index_buffer);
-			glBindBuffer(GL_DRAW_INDIRECT_BUFFER, mesh.draw_buffer);
+	next_scene = std::move(scene);
+	scene_dirty = true;
+}
 
-			// draw primitives
+void Renderer::update()
+{
+	// TODO: This is pretty expensive, could optimise later, or just leave it
+	// since updating the scene in this application is pretty rare.
+	if (scene_dirty) {
+		std::vector<DrawCommand> commands;
+		std::vector<Vertex> vertices;
+		std::vector<std::uint32_t> indices;
+
+		curr_scene = std::move(next_scene);
+		for (const auto& node : curr_scene.nodes) {
+			merge(commands, node.gltf.commands);
+			merge(vertices, node.gltf.vertices);
+			merge(indices, node.gltf.indices);
+		}
+		// TODO: check if this is optimal, or use glBufferData instead
+		if (vertex_buffer != GL_NONE) {
+			glDeleteBuffers(1, &vertex_buffer);
+			glDeleteBuffers(1, &index_buffer);
+			glDeleteBuffers(1, &command_buffer);
+		}
+		glCreateBuffers(1, &vertex_buffer);
+		glCreateBuffers(1, &index_buffer);
+		glCreateBuffers(1, &command_buffer);
+
+		glNamedBufferStorage(vertex_buffer, sizeof(Vertex) * vertices.size(), &vertices[0], GL_DYNAMIC_STORAGE_BIT);
+		glNamedBufferStorage(index_buffer, sizeof(std::uint32_t) * indices.size(), &indices[0], GL_DYNAMIC_STORAGE_BIT);
+		glNamedBufferStorage(command_buffer, sizeof(std::uint32_t) * commands.size(), &commands[0], GL_DYNAMIC_STORAGE_BIT);
+		scene_dirty = false;
+	}
+}
+
+void Renderer::render() const
+{
+	// bind global mesh buffers
+	glVertexArrayVertexBuffer(vao, 0, vertex_buffer, 0, sizeof(Vertex));
+	glVertexArrayElementBuffer(vao, index_buffer);
+	glBindBuffer(GL_DRAW_INDIRECT_BUFFER, command_buffer);
+	glBindBufferBase(GL_UNIFORM_BUFFER, 0, material_ubo);
+
+	for (auto& node : curr_scene.nodes) {
+		for (auto& meshnode : node.gltf.meshnodes) {
+			glUniformMatrix4fv(model_uniform, 1, GL_FALSE, &meshnode.transform[0][0]);
+			auto& mesh = node.gltf.meshes[meshnode.mesh_idx];
+
 			for (auto& primitive : mesh.primitives) {
-				glBindTextureUnit(0, primitive.albedo_texture);
-				glBindBufferBase(GL_UNIFORM_BUFFER, 0, mesh.materials[primitive.material_id]);
+				auto& material = node.gltf.materials[primitive.material_idx];
+				auto& texture = node.gltf.textures[primitive.texture_idx];
+
+				glBindTextureUnit(0, texture.id);
+				glNamedBufferSubData(material_ubo, 0, sizeof(Material), reinterpret_cast<const void*>(&material));
+
+				glMultiDrawElementsIndirect(GL_TRIANGLES, GL_UNSIGNED_INT, reinterpret_cast<const void*>(primitive.command_idx * sizeof(Primitive)), 1, 0);
 			}
 		}
-		glMultiDrawElementsIndirect(GL_TRIANGLES, GL_UNSIGNED_INT,
-			      reinterpret_cast<const void*>(i * sizeof(Primitive)), 0);
 	}
 }
--- src/renderer.h
+++ src/renderer.h
@@ -1,18 +1,29 @@
 #pragma once
 
+#include "scene.h"
+
 #include <fastgltf/types.hpp>
+#include <glad/gl.h>
 
 class Renderer {
+public:
 	Renderer(GLuint program);
+	void update_scene(Scene& scene);
 	void update();
-	void render(Scene& scene) const;
+	void render() const;
 private:
+	// gl buffers
+	GLuint vertex_buffer, index_buffer, command_buffer;
 	GLuint vao;
+
+	// scene data
+	bool scene_dirty = false;
+	Scene curr_scene;
+	Scene next_scene;
 	fastgltf::math::fmat4x4 view_projection;
 
-	// vertex shader
+	// uniforms and ubo
 	GLuint model_uniform;
 	GLuint view_proj_uniform;
-
-	// fragment shader
+	GLuint material_ubo;
 };
